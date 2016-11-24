53078d9440e3e88e9784eeeed9e150edc1301cf0
zengzag <zengzag@hotmail.com>
2016-11-24 15:05:48 +0800

    11

--- Bilateral/main.cpp
+++ Bilateral/main.cpp
@@ -293,8 +293,8 @@ static void on_mouse(int event, int x, int y, int flags, void* param)
 
 
 int main() {
-	video.open("image/parkour.avi");
-	videowriter.open("image/output.avi", CV_FOURCC('D', 'I', 'V', 'X'), 5, Size(video.get(CV_CAP_PROP_FRAME_WIDTH), video.get(CV_CAP_PROP_FRAME_HEIGHT)));
+	video.open("E:/Projects/OpenCV/DAVIS-data/image/paragliding.avi");
+	videowriter.open("E:/Projects/OpenCV/DAVIS-data/image/output.avi", CV_FOURCC('D', 'I', 'V', 'X'), 5, Size(video.get(CV_CAP_PROP_FRAME_WIDTH), video.get(CV_CAP_PROP_FRAME_HEIGHT)));
 	//CAP_PROP_FRAME_COUNT
 	for (int times = 0; times < 1; times++)
 	{
